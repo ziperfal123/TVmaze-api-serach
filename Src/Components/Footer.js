2da5bff23a7d5e01659e7cc52e58c3307753da7b
Yaniv Ziperfal <ziperfal123@gmail.com>
2019-07-14 17:39:40 +0300

    style change

--- Src/Components/Footer.js
+++ Src/Components/Footer.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Dimensions } from "react-native";
 const styles = StyleSheet.create({
   footerStyle: {
     height: Dimensions.get("window").height * 0.075,
-    backgroundColor: "#f1f4df"
+    backgroundColor: "#E6E8EE"
   },
   textStyle: {
     fontSize: 20,
